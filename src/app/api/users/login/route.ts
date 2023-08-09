f0f7e9fa11d02402820b06d27336a1439d142c05
Ezeh20 <ranter1570@gmail.com>
2023-08-09 17:11:27 +0100

    login and signup route completed

--- src/app/api/users/login/route.ts
+++ src/app/api/users/login/route.ts
@@ -2,35 +2,53 @@ import User from "@/models/User-model";
 import connect from "@/utils/dbConfig";
 import { NextResponse, NextRequest } from "next/server";
 import bcryptjs from "bcrypt";
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 
 export const POST = async (request: NextRequest) => {
-  await connect();
   const body = await request.json();
   const { email, password } = body;
+  await connect();
 
   try {
     //check if the user exists
     const user = await User.findOne({ email });
 
+    console.log(user);
     if (!user) {
-      return NextResponse.json({ error: "Wrong email or password" }, {status:400});
+      return NextResponse.json(
+        { error: "Wrong username or password" },
+        { status: 400 }
+      );
     }
-    //check the password
+
+    //check if the user's password is correct
     const isPasswordValid = await bcryptjs.compare(password, user.password);
-
-    if(!isPasswordValid){
-        return NextResponse.json({error: "Wrong email or password" }, {status:400})
+    if (!isPasswordValid) {
+      return NextResponse.json(
+        { error: "Wrong username or password" },
+        { status: 400 }
+      );
     }
 
-    //create a tokenData to be sent to the client
-    const tokenData ={
-        id:user._id,
-        username:user.username,
-        email:user.email,
-    }
+    //extract the user data you need to send to the client
+    const userData = {
+      id: user._id,
+      username: user.username,
+      email: user.email,
+    };
 
+    //create a token for the client
+    const token = jwt.sign(userData, process.env.TOKEN_SECRET!, {
+      expiresIn: "1d",
+    });
 
+    //response
+    const response = NextResponse.json(
+      { message: "Login successful" },
+      { status: 200 }
+    );
+    response.cookies.set("token", token, { httpOnly: true });
+    return response;
   } catch (error: any) {
     return NextResponse.json({ error: error.message }, { status: 500 });
   }
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,19 +1,34 @@
 "use client"
-
 import React from 'react'
 import axios from 'axios'
 import Link from 'next/link'
-import useRouter from 'next/navigation'
+import {useRouter} from 'next/navigation'
 
 const SignUp = () => {
  const initialState = {
   email:'',
   password:'',
  }
+ const router = useRouter()
   const [userData, setUserData] = React.useState(initialState)
+  const [loading, setLoading] = React.useState(false)
+  
 
   const handleSubmit = async (e:any) =>{
     e.preventDefault()
+    try {
+      setLoading(true)
+      const {data} = await axios.post("/api/users/login", userData)
+      console.log(data);
+      if(!data.error){
+        router.push('/profile')
+      }
+      setLoading(false)
+    } catch (error) {
+      setLoading(false)
+    }finally{
+      setLoading(false)
+    }
   }
 
   return (
@@ -37,7 +52,11 @@ const SignUp = () => {
           />
         </div>
         <div className=' flex flex-col gap-5 justify-center items-center'>
-        <button className=' bg-slate-50 text-slate-900 w-full p-2 rounded-md'>SignUp</button>
+        <button className=' bg-slate-50 text-slate-900 w-full p-2 rounded-md'>
+          {
+            loading ? 'Loading..' : 'Login'
+          }
+        </button>
         <Link href='/signup'>Don`t have an account?</Link>
         </div>
       </form>
